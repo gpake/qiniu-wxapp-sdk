096146414e71a58dc98d8fcfa065657269fe637c
Michael Xue <gpake@163.com>
2017-05-25 21:13:26 +0800

    update sdk directory files

--- sdk/qiniuUploader.js
+++ sdk/qiniuUploader.js
@@ -2,11 +2,12 @@
 (function() {
 
 var config = {
-    qiniuUploadURL: '',
+    qiniuRegion: '',
     qiniuImageURLPrefix: '',
     qiniuUploadToken: '',
     qiniuUploadTokenURL: '',
-    qiniuUploadTokenFunction: null
+    qiniuUploadTokenFunction: null,
+    qiniuShouldUseQiniuFileName: false
 }
 
 module.exports = {
@@ -18,20 +19,21 @@ module.exports = {
 // 如果需要变更参数，再调用 init 即可
 function init(options) {
     config = {
-        qiniuUploadURL: '',
+        qiniuRegion: '',
         qiniuImageURLPrefix: '',
         qiniuUploadToken: '',
         qiniuUploadTokenURL: '',
-        qiniuUploadTokenFunction: null
+        qiniuUploadTokenFunction: null,
+        qiniuShouldUseQiniuFileName: false
     };
     updateConfigWithOptions(options);
 }
 
 function updateConfigWithOptions(options) {
-    if (options.uploadURL) {
-        config.qiniuUploadURL = options.uploadURL;
+    if (options.region) {
+        config.qiniuRegion = options.region;
     } else {
-        console.error('qiniu uploader need uploadURL');
+        console.error('qiniu uploader need your bucket region');
     }
     if (options.uptoken) {
         config.qiniuUploadToken = options.uptoken;
@@ -43,6 +45,7 @@ function updateConfigWithOptions(options) {
     if (options.domain) {
         config.qiniuImageURLPrefix = options.domain;
     }
+    config.qiniuShouldUseQiniuFileName = options.shouldUseQiniuFileName
 }
 
 function upload(filePath, success, fail, options) {
@@ -51,7 +54,7 @@ function upload(filePath, success, fail, options) {
         return;
     }
     if (options) {
-        init(options);
+      updateConfigWithOptions(options);
     }
     if (config.qiniuUploadToken) {
         doUpload(filePath, success, fail, options);
@@ -61,6 +64,10 @@ function upload(filePath, success, fail, options) {
         });
     } else if (config.qiniuUploadTokenFunction) {
         config.qiniuUploadToken = config.qiniuUploadTokenFunction();
+        if (null == config.qiniuUploadToken && config.qiniuUploadToken.length > 0) {
+            console.error('qiniu UploadTokenFunction result is null, please check the return value');
+            return
+        }
     } else {
         console.error('qiniu uploader need one of [uptoken, uptokenURL, uptokenFunc]');
         return;
@@ -68,15 +75,21 @@ function upload(filePath, success, fail, options) {
 }
 
 function doUpload(filePath, success, fail, options) {
-    var url = config.qiniuUploadURL;
+    if (null == config.qiniuUploadToken && config.qiniuUploadToken.length > 0) {
+        console.error('qiniu UploadToken is null, please check the init config or networking');
+        return
+    }
+    var url = uploadURLFromRegionCode(config.qiniuRegion);
     var fileName = filePath.split('//')[1];
     if (options && options.key) {
         fileName = options.key;
     }
     var formData = {
-        'token': config.qiniuUploadToken,
-        'key': fileName
+        'token': config.qiniuUploadToken
     };
+    if (!config.qiniuShouldUseQiniuFileName) {
+      formData['key'] = fileName
+    }
     wx.uploadFile({
         url: url,
         filePath: filePath,
@@ -86,14 +99,18 @@ function doUpload(filePath, success, fail, options) {
             var dataString = res.data
             var dataObject = JSON.parse(dataString);
             //do something
-            var imageUrl = config.qiniuImageURLPrefix + dataObject.key;
+            var imageUrl = config.qiniuImageURLPrefix + '/' + dataObject.key;
             dataObject.imageURL = imageUrl;
             console.log(dataObject);
-            success(dataObject);
+            if (success) {
+                success(dataObject);
+            }
         },
         fail: function (error) {
-            console.log(error);
-            fail(error);
+            console.error(error);
+            if (fail) {
+                fail(error);
+            }
         }
     })
 }
@@ -103,15 +120,31 @@ function getQiniuToken(callback) {
     url: config.qiniuUploadTokenURL,
     success: function (res) {
       var token = res.data.uptoken;
-      config.qiniuUploadToken = token;
-      if (callback) {
-          callback();
+      if (token && token.length > 0) {
+        config.qiniuUploadToken = token;
+        if (callback) {
+            callback();
+        }
+      } else {
+        console.error('qiniuUploader cannot get your token, please check the uptokenURL or server')
       }
     },
     fail: function (error) {
-      console.log(error);
+      console.error('qiniu UploadToken is null, please check the init config or networking: ' + error);
     }
   })
+}
+
+function uploadURLFromRegionCode(code) {
+    var uploadURL = null;
+    switch(code) {
+        case 'ECN': uploadURL = 'https://up.qbox.me'; break;
+        case 'NCN': uploadURL = 'https://up-z1.qbox.me'; break;
+        case 'SCN': uploadURL = 'https://up-z2.qbox.me'; break;
+        case 'NA': uploadURL = 'https://up-na0.qbox.me'; break;
+        default: console.error('please make the region is with one of [ECN, SCN, NCN, NA]');
+    }
+    return uploadURL;
 }
 
 })();
